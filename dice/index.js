71de925eda31577a4d8dc67d34952e6b6f2b990e
kingmatayo <fox256.labs@gmail.com>
2024-12-30 22:57:50 +0300

    correct final game

--- dice/index.js
+++ dice/index.js
@@ -1,8 +1,21 @@
 var randomNumber1 = Math.floor(Math.random()*5)+1
 
+var randomNumber2 = Math.floor(Math.random()*5)+1
+
+
 var imageStringstart = "images/dice"
 
 var imageStringend = ".png"
 
 
 document.querySelector(".img1").setAttribute("src",imageStringstart + randomNumber1 + imageStringend);
+
+document.querySelector(".img2").setAttribute("src",imageStringstart + randomNumber2 + imageStringend);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 has won!"
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 has won!"
+} else {
+  document.querySelector("h1").innerHTML = "Its a Draw!"  
+}
