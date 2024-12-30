d9646058e0ae42f06fdea89de467b07fb2d1b79e
kingmatayo <fox256.labs@gmail.com>
2024-12-30 23:35:39 +0300

    Updated Math.Random in index.js to * 6

--- dice/index.js
+++ dice/index.js
@@ -1,6 +1,6 @@
-var randomNumber1 = Math.floor(Math.random()*5)+1
+var randomNumber1 = Math.floor(Math.random()*6)+1
 
-var randomNumber2 = Math.floor(Math.random()*5)+1
+var randomNumber2 = Math.floor(Math.random()*6)+1
 
 
 var imageStringstart = "images/dice"
